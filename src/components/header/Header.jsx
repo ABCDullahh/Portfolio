e4b6648f10aae5c68514f76591a8ebc3a3998545
Faizal Lutfi Yoga Triadi <42670670+ABCDullahh@users.noreply.github.com>
2022-12-12 00:51:38 +0700

    bug fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,47 +1,47 @@
-import React from 'react'
-import "./header.css"
+import React from 'react';
+import "./header.css";
 
 const Header = () => {
   return (
     <header className="header">
       <nav className="nav container">
         <a href="index.html" className="nav__logo">Faizal</a>
-        
+
         <div className="nav__menu">
           <ul className="nav__list grid">
             <li className="nav__item">
               <a href="#home" className="nav__link active-link">
-                <i className="uil uil-estate nav__icon"> Home </i>
+                <i className="uil uil-estate nav__icon"></i>Home
               </a>
             </li>
 
             <li className="nav__item">
               <a href="#about" className="nav__link">
-                <i className="uil uil-user nav__icon"> About </i>
+                <i className="uil uil-user nav__icon"></i>About
               </a>
             </li>
 
             <li className="nav__item">
               <a href="#skills" className="nav__link">
-                <i className="uil uil-file-alt nav__icon"> Skills </i>
+                <i className="uil uil-file-alt nav__icon"></i>Skills
               </a>
             </li>
 
             <li className="nav__item">
               <a href="#services" className="nav__link">
-                <i className="uil uil-briefcase-alt nav__icon"> Services </i>
+                <i className="uil uil-briefcase-alt nav__icon"></i>Services
               </a>
             </li>
 
             <li className="nav__item">
               <a href="#portfolio" className="nav__link">
-                <i className="uil uil-scenery nav__icon"> Portfolio </i>
+                <i className="uil uil-scenery nav__icon"></i>Portfolio
               </a>
             </li>
 
             <li className="nav__item">
               <a href="#contact" className="nav__link">
-                <i className="uil uil-message nav__icon"> Contact </i>
+                <i className="uil uil-message nav__icon"></i>Contact
               </a>
             </li>
           </ul>
